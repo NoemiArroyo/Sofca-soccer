233c56b935ca23ce74b472aea075bb8b2bee62ed
Vivis B <barriosvivis44@gmail.com>
2024-08-23 15:01:15 -0500

    transaccion completa

--- src/main/java/com/softca/soccer/dao/TransaccionDao.java
+++ src/main/java/com/softca/soccer/dao/TransaccionDao.java
@@ -5,11 +5,13 @@ import com.softca.soccer.dto.Transacciones;
 import com.softca.soccer.exception.DaoException;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TransaccionDao {
     public void insert(Transacciones transaccion) throws DaoException;
     public void update(Transacciones transaccion) throws DaoException;
     public void delete(Transacciones transaccion) throws DaoException;
     public Transacciones selectById( Transacciones transaccion) ;
-    public List<Transacciones > selectAll() throws DaoException;
+    public List<Map<String, Object>> selectAll() throws DaoException;
+
 }
